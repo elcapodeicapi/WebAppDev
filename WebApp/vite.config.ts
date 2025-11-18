e0f09dedb5b4f1ef51181dc45e1d6f5acc0da5de
ZiyabRaza001 <chanda.zr@gmail.com>
2025-11-18 13:01:46 +0100

    npm run dev runt nu ook de api

--- WebApp/vite.config.ts
+++ WebApp/vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Proxy the swagger UI and JSON from the backend so you can open
+      // http://localhost:5173/swagger and see the API docs served by the API.
+      '/swagger': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+        // keep the path as-is so /swagger and /swagger/v1/swagger.json map correctly
+        rewrite: (path) => path.replace(/^\/swagger/, '/swagger')
+      }
+    }
+  }
 })
